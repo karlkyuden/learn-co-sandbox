9f863363c3ba743ebe43f6255cf851d8f75550f5
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-14 06:41:32 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,8 @@
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+ 
+spice_rack[2] << "Saffron" #=> ["Parsley", "Sage", "Rosemary", "Saffron"]
+ 
